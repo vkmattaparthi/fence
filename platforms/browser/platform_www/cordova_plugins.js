d44ab269a4f04a109c2acab8ac992e641a93e126
Venkatesh Mattaparthi <mattaparthi.venkatesh@gmail.com>
2020-04-25 11:58:37 +0530

    inital commit

---
+++ platforms/browser/platform_www/cordova_plugins.js
@@ -0,0 +1,111 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "file": "plugins/cordova-plugin-device/www/device.js",
+        "id": "cordova-plugin-device.device",
+        "pluginId": "cordova-plugin-device",
+        "clobbers": [
+            "device"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-device/src/browser/DeviceProxy.js",
+        "id": "cordova-plugin-device.DeviceProxy",
+        "pluginId": "cordova-plugin-device",
+        "runs": true
+    },
+    {
+        "file": "plugins/cordova-plugin-ionic-webview/src/www/util.js",
+        "id": "cordova-plugin-ionic-webview.IonicWebView",
+        "pluginId": "cordova-plugin-ionic-webview",
+        "clobbers": [
+            "Ionic.WebView"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-splashscreen/www/splashscreen.js",
+        "id": "cordova-plugin-splashscreen.SplashScreen",
+        "pluginId": "cordova-plugin-splashscreen",
+        "clobbers": [
+            "navigator.splashscreen"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-splashscreen/src/browser/SplashScreenProxy.js",
+        "id": "cordova-plugin-splashscreen.SplashScreenProxy",
+        "pluginId": "cordova-plugin-splashscreen",
+        "runs": true
+    },
+    {
+        "file": "plugins/cordova-plugin-statusbar/www/statusbar.js",
+        "id": "cordova-plugin-statusbar.statusbar",
+        "pluginId": "cordova-plugin-statusbar",
+        "clobbers": [
+            "window.StatusBar"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-statusbar/src/browser/StatusBarProxy.js",
+        "id": "cordova-plugin-statusbar.StatusBarProxy",
+        "pluginId": "cordova-plugin-statusbar",
+        "runs": true
+    },
+    {
+        "file": "plugins/WebViewer/www/webview.js",
+        "id": "WebViewer.webview",
+        "pluginId": "WebViewer",
+        "clobbers": [
+            "webview"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-nativegeocoder/www/NativeGeocoder.js",
+        "id": "cordova-plugin-nativegeocoder.NativeGeocoder",
+        "pluginId": "cordova-plugin-nativegeocoder",
+        "clobbers": [
+            "nativegeocoder"
+        ]
+    },
+    {
+        "file": "plugins/es6-promise-plugin/www/promise.js",
+        "id": "es6-promise-plugin.Promise",
+        "pluginId": "es6-promise-plugin",
+        "runs": true
+    },
+    {
+        "file": "plugins/cordova-plugin-geofence/www/TransitionType.js",
+        "id": "cordova-plugin-geofence.TransitionType",
+        "pluginId": "cordova-plugin-geofence",
+        "clobbers": [
+            "TransitionType"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-geofence/www/geofence.js",
+        "id": "cordova-plugin-geofence.geofence",
+        "pluginId": "cordova-plugin-geofence",
+        "clobbers": [
+            "geofence"
+        ]
+    }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+    "cordova-plugin-device": "2.0.2",
+    "cordova-plugin-ionic-keyboard": "2.2.0",
+    "cordova-plugin-ionic-webview": "4.2.0",
+    "cordova-plugin-splashscreen": "5.0.2",
+    "cordova-plugin-statusbar": "2.4.2",
+    "cordova-plugin-whitelist": "1.3.3",
+    "cordova-plugin-androidx": "1.0.2",
+    "WebViewer": "0.0.1",
+    "cordova-plugin-geolocation": "4.0.2",
+    "cordova-android-support-gradle-release": "3.0.1",
+    "cordova-plugin-nativegeocoder": "3.4.0",
+    "cordova-plugin-compat": "1.2.0",
+    "es6-promise-plugin": "4.2.2",
+    "cordova-plugin-geofence": "0.7.0"
+}
+// BOTTOM OF METADATA
+});
---
+++ src/app/tab1/tab1.page.ts
@@ -0,0 +1,105 @@
+import { Component, ViewChild, ElementRef} from '@angular/core';
+import { Geofence } from '@ionic-native/geofence/ngx';
+import { Geolocation } from '@ionic-native/geolocation/ngx';
+import { NativeGeocoder, NativeGeocoderResult, NativeGeocoderOptions } from '@ionic-native/native-geocoder/ngx';
+
+declare var google;
+
+@Component({
+  selector: 'app-tab1',
+  templateUrl: 'tab1.page.html',
+  styleUrls: ['tab1.page.scss']
+})
+export class Tab1Page {
+
+  @ViewChild('map', {static:false}) mapElement: ElementRef;
+  map: any;
+  address:string;
+
+  constructor(private geofence: Geofence, private geolocation: Geolocation, private nativeGeocoder: NativeGeocoder) {
+    
+  }
+
+  ngOnInit() {
+    this.loadMap();
+  }
+
+  loadMap() {
+    this.geolocation.getCurrentPosition().then((resp) => {
+      alert('triggered again');
+      let latLng = new google.maps.LatLng(resp.coords.latitude, resp.coords.longitude);
+      let mapOptions = {
+      center: latLng,
+      zoom: 15,
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    }
+    this.getAddressFromCoords(resp.coords.latitude, resp.coords.longitude);
+
+    this.map = new google.maps.Map(this.mapElement.nativeElement, mapOptions);
+
+    this.map.addListener('tilesloaded', () => {
+      console.log('accuracy',this.map);
+      this.getAddressFromCoords(this.map.center.lat(), this.map.center.lng())
+    });
+      this.addGeofence(1, 123-323, resp.coords.latitude, resp.coords.longitude, 'Hyderabad', 'home');
+     }).catch((error) => {
+       alert('Error getting location : ' + JSON.stringify(error));
+     });
+  }
+
+  getAddressFromCoords(lattitude, longitude) {
+    console.log("getAddressFromCoords "+lattitude+" "+longitude);
+    let options: NativeGeocoderOptions = {
+      useLocale: true,
+      maxResults: 5
+    };
+
+    this.nativeGeocoder.reverseGeocode(lattitude, longitude, options)
+      .then((result: NativeGeocoderResult[]) => {
+        this.address = "";
+        let responseAddress = [];
+        for (let [key, value] of Object.entries(result[0])) {
+          if(value.length>0)
+          responseAddress.push(value);
+
+        }
+        responseAddress.reverse();
+        for (let value of responseAddress) {
+          this.address += value+", ";
+        }
+        this.address = this.address.slice(0, -2);
+      })
+      .catch((error: any) =>{ 
+        this.address = "Address Not Available!";
+        alert("Address Not Available! : " + JSON.stringify(error));
+      });
+
+  }
+
+
+  private addGeofence(id, idx, lat, lng, place, desc) {
+    let fence = {
+      id: id,
+      latitude: lat,
+      longitude: lng,
+      radius: 50,
+      transitionType: 2,
+      notification: {
+          id: idx,
+          title: 'You crossed ' + place,
+          text: desc,
+          openAppOnClick: true
+      }
+    }
+  
+    this.geofence.addOrUpdate(fence).then(
+       () => alert('Geofence added'),
+       (err) => alert('Geofence failed to add' + JSON.stringify(err))
+     );
+  }
+
+  addFence() {
+    alert();
+  }
+
+}
